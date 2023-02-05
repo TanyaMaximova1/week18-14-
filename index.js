213a6d99d7fe3196cadd2f1db3c6467b12cd7611
TanyaMaximova1 <tatamurom@gmail.com>
2023-02-06 00:06:15 +0300

    step mas

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
-// let mas = new Array(2);
-// mas[3] = 5;
-// console.log(mas[3]);
-
-
-// let mas = new Array(2);
-// mas[3] = 5;
-// console.log(mas3);
-
-// let a = new Array(2);
-// a[1] = null;
-// console.log(a.length);
-
-// const students = ["Lena", "Olya", "Ylia", "Roma", "Vova"];
-// console.log(students[3]);
-
-// const fruits = ["Груша", "Яблоко", "Лимон", "Ананас",];
-// console.log(fruits.shift());
-// console.log(fruits);
-
-let textValue = document.getElementsByName("field");
-console.log(textValue[0].value);
+let directors = [
+    {
+        name: 'Стивен Спилберг',
+        career: 'Продюсер, Режиссёр, Актёр, Сценарист, Монтажёр',
+        films: 'https://ru.wikipedia.org/wiki/%D0%A4%D0%B8%D0%BB%D1%8C%D0%BC%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D1%8F_%D0%A1%D1%82%D0%B8%D0%B2%D0%B5%D0%BD%D0%B0_%D0%A1%D0%BF%D0%B8%D0%BB%D0%B1%D0%B5%D1%80%D0%B3%D0%B0',
+        top_rated_film: 'Список Шиндлера'
+    },
+    {
+        name: 'Кристофер Нолан',
+        career: 'Сценарист, Продюсер, Режиссёр, Оператор, Монтажёр, Актёр, Композитор',
+        films: 'https://www.film.ru/compilation/vse-filmy-kristofera-nolana-ot-luchshego-k-horoshemu',
+        top_rated_film: 'Начало'
+    },
+    {
+        name: 'Мартин МакДона',
+        career: 'Сценарист, Режиссёр, Продюсер',
+        films: 'https://www.film.ru/person/martin-makdonah',
+        top_rated_film: 'Три билборда на границе Эббинга, Миссури'
+    },
+    {
+        name: 'Алексей Балабанов',
+        career: 'Режиссёр, Сценарист, Актёр, Продюсер',
+        films: 'https://www.film.ru/person/aleksey-balabanov',
+        top_rated_film: 'Брат'
+    },
+    {
+        name: 'Питер Фаррелли',
+        career: 'Продюсер, Режиссёр, Сценарист, Актёр',
+        films: 'https://www.film.ru/person/piter-farrelli',
+        top_rated_film: 'Зелёная книга'
+    },
+    {
+        name: 'Юрий Быков',
+        career: 'Актёр, Режиссёр, Сценарист, Композитор, Монтажёр, Продюсер',
+        films: 'https://www.film.ru/person/yuriy-bykov',
+        top_rated_film: 'Дурак'
+    },
+    {
+        name: 'Жан-Марк Валле',
+        career: 'Режиссер, Продюсер, Монтажёр, Актёр, Сценарист',
+        films: 'https://www.film.ru/person/zhan-mark-valle',
+        top_rated_film: 'Далласский клуб покупателей'
+    },
+];
